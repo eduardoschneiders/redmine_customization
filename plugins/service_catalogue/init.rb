82a8b24dfb8ef0e1c1792354e56c103926328475
eduardo.schneiders <eduardo.schneiders@cwi.com.br>
2016-11-18 14:41:31 -0200

    Default plugin

---
+++ plugins/service_catalogue/init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :service_catalogue do
+  name 'Service Catalogue plugin'
+  author 'Eduardo Schneiders'
+  description 'This adds a catalogue of services'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
